ce3dd68e51174189f46a8a8cab969577e07c5eea
lakarim <leomessinger7@gmail.com>
2023-05-29 19:13:02 -0700

    add a ship class

--- ailean_invasion.py
+++ ailean_invasion.py
@@ -1,18 +1,21 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AilenInvasion:
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
+        
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width,
              self.settings.screen_height
             )
         )
         pygame.display.set_caption(self.settings.caption)
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
@@ -21,6 +24,8 @@ class AilenInvasion:
                     sys.exit()
 
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            
             pygame.display.flip()
             self.clock.tick(60)
 
